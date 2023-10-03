35e3105e9bbe38a29c721e7b5e6fd7dc9ddd8ea3
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-10-03 13:13:29 +0100

    customer entity/repository

--- Customer-Service/src/main/java/com/houd/customerservice/CustomerServiceApplication.java
+++ Customer-Service/src/main/java/com/houd/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package com.houd.customerservice;
 
+import com.houd.customerservice.entities.Customer;
+import com.houd.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +13,16 @@ public class CustomerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.save(new Customer(null,"Fatiza","fatiza@mail.com"));
+			customerRepository.save(new Customer(null,"Hind","hind@mail.com"));
+			customerRepository.save(new Customer(null,"Majda","majda@mail.com"));
 
+			customerRepository.findAll().forEach(customer -> {
+				System.out.println(customer.toString());
+			});
+		};
+	}
 }
